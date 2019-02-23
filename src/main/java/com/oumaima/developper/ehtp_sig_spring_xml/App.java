19a6693a4e2d793d602abb3668616b38b61b1d77
Oumaima <o.moussawi04@gmail.com>
2019-02-23 09:56:35 +0100

    First commit

---
+++ src/main/java/com/oumaima/developper/ehtp_sig_spring_xml/App.java
@@ -0,0 +1,13 @@
+package com.oumaima.developper.ehtp_sig_spring_xml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
